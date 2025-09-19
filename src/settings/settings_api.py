52857f5164a1477714d2dfeb87382feeef3720ca
MarcuseDemarchi <marcus.demarchi@unidavi.edu.br>
2025-09-18 21:25:28 -0300

    feat(settings) : adiciona classe de configurações

---
+++ src/settings/settings_api.py
@@ -0,0 +1,9 @@
+import os
+from src.settings.settings_conf import PATH_DOTENV
+from dotenv import load_dotenv
+
+
+## Carrega configuração da API
+class API():
+    load_dotenv(PATH_DOTENV)
+    API_KEY = os.getenv('API_KEY')
---
+++ src/settings/settings_conf.py
@@ -0,0 +1,6 @@
+import os
+from pathlib import Path
+
+PATH_ROOT  = Path(__file__).parents[2]
+PATH_DOTENV = PATH_ROOT / '.env'
+PATH_ASSETS = PATH_ROOT / 'src' / 'assets'
